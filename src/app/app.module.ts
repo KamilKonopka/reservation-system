0a0c173136e0a12f23f8afb8dec6bce7600d9469
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-04-08 18:12:40 +0200

    created new components: header, main, footer, crousel, login, register, regulations, about-us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { RegulaminComponent } from './regulamin/regulamin.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { RegisterComponent } from './register/register.component';
+import { RegulationsComponent } from './regulations/regulations.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    LoginComponent,
+    CarouselComponent,
+    RegulaminComponent,
+    AboutUsComponent,
+    RegisterComponent,
+    RegulationsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/regulations/regulations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regulations',
+  templateUrl: './regulations.component.html',
+  styleUrls: ['./regulations.component.less']
+})
+export class RegulationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
