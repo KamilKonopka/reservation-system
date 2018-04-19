eb7752c8b10b5e2ba5ad136bac698fbca511ba54
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-04-19 19:58:22 +0200

    simplified animation event

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
@@ -8,7 +9,17 @@ import * as $ from 'jquery';
 export class PageNotFoundComponent implements OnInit {
 
   constructor() { }
-
+  jumpAround () {
+    $('h1').animate({
+        width: 'toggle',
+        height: 'toggle'
+      }, {
+        duration: 1000,
+        complete: function() {
+        $( this ).after( '<div>Page does not exist!</div>' );
+        }
+      });
+    }
   ngOnInit() {
   }
 
