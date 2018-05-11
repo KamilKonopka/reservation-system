56bede5ad6eeb67fcda36765f0249590309191df
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-05-11 16:39:51 +0200

    added read: users to auth serve

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import * as auth0 from 'auth0-js';
 import { Router } from '@angular/router';
 
-
-
 @Injectable()
 export class AuthService {
   userProfile: any;
@@ -13,7 +11,8 @@ export class AuthService {
     responseType: 'token id_token',
     audience: 'https://trutnie.eu.auth0.com/userinfo',
     redirectUri: 'http://localhost:4200/logged',
-    scope: 'openid profile'
+    scope: 'openid profile',
+    read: 'users'
   });
   constructor(public router: Router) { }
 
