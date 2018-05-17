47d9c2b6acbe5cbf1731ff52c5220ebd08afe5f8
Kamil <kamil.konopka@thomsonreuters.com>
2018-05-17 09:38:35 +0200

    Revert "Merge branch 'feature-retype-pass' into 'develop'"
    This reverts merge request !24

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -39,8 +39,8 @@ export class RegisterFormComponent  implements OnInit {
     this.user.ulica = this.myform.value.ulica;
   this.user.email  = this.myform.value.email;
   // this.myname = this.myform.controls.name.value;
-  this.user.imie = this.myform.value.firstName;
-  this.user.name = this.myform.value.lastName;
+  this.user.imie = this.myform.value.name.firstName;
+  this.user.name = this.myform.value.name.lastName;
   this.user.nrdomu = this.myform.value.nrdomu;
   this.user.telefon = this.myform.value.phoneNo;
   this.user.haslo = this.myform.value.password;
@@ -70,10 +70,12 @@ export class RegisterFormComponent  implements OnInit {
 
     ngOnInit() {
     this.myform = new FormGroup({
-    firstName: new FormControl('', [
-    Validators.required
-    ]),
-    lastName: new FormControl('', Validators.required),
+      name: new FormGroup({
+firstName: new FormControl('', [
+  Validators.required
+]),
+lastName: new FormControl('', Validators.required),
+  }),
       email: new FormControl('', [
         Validators.required,
         Validators.pattern('[^ @]*@[^ @]*')
@@ -87,10 +89,6 @@ export class RegisterFormComponent  implements OnInit {
         Validators.minLength(3)
       ]),
       password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6)
-      ]),
-      repassword: new FormControl('', [
         Validators.required,
         Validators.minLength(6)
       ]),
