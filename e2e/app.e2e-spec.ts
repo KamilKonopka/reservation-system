ee88365862f26813067a8ce99d63ae76b6acc8a8
kamil.konopka <kamil.konopka@thomsonreuters.com>
2018-06-23 11:17:22 +0200

    added e2e test for starting page

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,14 @@ describe('reservation-system App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display carousel h2 text', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getH2Text()).toEqual('ZAREZERWUJ BOISKO');
   });
+
+  it('should display carousel paragraph text', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Sprawdź dostępne obiekty i preferowane terminy.');
+  });
+
 });
